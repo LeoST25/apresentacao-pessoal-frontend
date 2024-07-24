716724af5fca591686e55a7f0eed95baa8987daf
LeoST25 <matheusleonardo224@gmail.com>
2024-07-24 18:39:52 -0300

    FEAT: ajustando configuração e corrigindo texto

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
         <h2 className="text-4xl mb-4 font-semibold">Projeto Recente</h2>
         <ul className="space-y-4">
             <h3 className="text-2xl font-bold">Projeto Hospedagem Hotel</h3>
-            <p className="text-lg mt-2">Criar um sistema de hospedagem de hotel utilizando o conceito de POO da linguagem C# com framework .NET, o intuito era utilizar os conceitos de herança, polimorfismo, abstação e encapsulamento aprendidos durante o curso. Link do repositório: <a href="https://github.com/LeoST25/Projeto-hospedagem-hotel"className="link-click">Projeto Hopedagem Hotal</a></p>
+            <p className="text-lg mt-2">Criar um sistema de hospedagem de hotel utilizando o conceito de POO da linguagem C# com framework .NET, o intuito era utilizar os conceitos de herança, polimorfismo, abstação e encapsulamento aprendidos durante o curso. Link do repositório: <a href="https://github.com/LeoST25/Projeto-hospedagem-hotel"className="link-click">Projeto Hopedagem Hotel</a></p>
         </ul>
       </section>
     );
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   base: '/apresentacao-pessoal-frontend/',
+  plugins: [react()],
   // outras configurações
 })
