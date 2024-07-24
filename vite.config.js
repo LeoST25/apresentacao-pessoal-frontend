f0ff5e081c9498b310793babb7c3a3d23fb1a111
LeoST25 <matheusleonardo224@gmail.com>
2024-07-24 19:38:23 -0300

    update vite config base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: 'https://leost25.github.io/apresentacao-pessoal-frontend/',
+  base: 'https://leost25.github.io/',
   plugins: [react()],
   build: {
     outDir: 'dist'
