5f010564136e7e13f869c4d7493c1cf46908539a
LeoST25 <matheusleonardo224@gmail.com>
2024-07-24 19:23:52 -0300

    Rebuild e Deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import myImage from '../assets/leo.png'
 
 const Header = () => {
   return (
     <header className="header bg-blue-600 text-white p-4 text-center">
-      <img src="/src/assets/foto-fundo-preto-leo.png" alt="Profile" className="profile-img" />
+      <img src= {myImage} alt="Profile" className="profile-img"/>
       <h1 className="text-4xl">Leonardo Matheus</h1>
       <p className="text-xl">Desenvolvedor Backend Jr</p>
     </header>
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/apresentacao-pessoal-frontend/',
+  base: 'https://leost25.github.io/apresentacao-pessoal-frontend/',
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
   // outras configurações
 })
